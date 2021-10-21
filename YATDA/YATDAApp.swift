a61e2b738ba0ff4fd5976b04381887f04445b418
Scott Bolin <sbolin@gmail.com>
2021-10-22 00:53:06 +0700

    Initial Commit

---
+++ YATDA/YATDAApp.swift
@@ -0,0 +1,17 @@
+//
+//  YATDAApp.swift
+//  YATDA
+//
+//  Created by Scott Bolin on 22-Oct-21.
+//
+
+import SwiftUI
+
+@main
+struct YATDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
